3e9bf25705022820a68c57a21c0b1f74858a9718
dimakrych <163765655+dimakrych@users.noreply.github.com>
2026-01-10 15:06:39 +0200

    Update logo source URLs based on language

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ import { useLanguage } from '../context/LanguageContext';
 export const Logo: React.FC<{ className?: string }> = ({ className = "h-12" }) => {
   const { language } = useLanguage();
   
-  const logoSrc = language === 'en' ? '/logo_en.png' : '/logo.png';
+  const logoSrc = language === 'en' ? 'https://firebasestorage.googleapis.com/v0/b/kcyc-site.firebasestorage.app/o/LOGO%20KCYC%2FLogo%20ENG%20color.png?alt=media&token=41a8a342-a17a-424d-af11-d0bd60b1ca12' : 'https://firebasestorage.googleapis.com/v0/b/kcyc-site.firebasestorage.app/o/LOGO%20KCYC%2FGroup%206680.png?alt=media&token=2da4c663-70f3-4415-90bf-458feae55e30';
 
   return (
     <img 
