3f8429c710d4b351ca801fb2eff27723ae10836d
dimakrych <163765655+dimakrych@users.noreply.github.com>
2026-01-11 17:27:43 +0200

    Update logo source URLs for different themes and languages

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -14,9 +14,9 @@ export const Logo: React.FC<{ className?: string }> = ({ className = "h-12" }) =
   
   const getLogoSrc = () => {
     if (language === 'en') {
-      return theme === 'dark' ? '/logo_en_light.png' : '/logo_en.png';
+      return theme === 'dark' ? 'https://firebasestorage.googleapis.com/v0/b/kcyc-site.firebasestorage.app/o/LOGO%20KCYC%2FLogo%20ENG%20color%20(1).png?alt=media&token=b789d3d7-6394-4ef7-a404-521e30bea5a0' : 'https://firebasestorage.googleapis.com/v0/b/kcyc-site.firebasestorage.app/o/LOGO%20KCYC%2FLogo%20ENG%20color.png?alt=media&token=41a8a342-a17a-424d-af11-d0bd60b1ca12';
     } else {
-      return theme === 'dark' ? '/logo_light.png' : '/logo.png';
+      return theme === 'dark' ? 'https://firebasestorage.googleapis.com/v0/b/kcyc-site.firebasestorage.app/o/LOGO%20KCYC%2FLogo%20%D0%9A%D0%9C%D0%9C%D0%A0%20ua%20color.png?alt=media&token=01278564-fec1-45fc-8777-0451d0351864' : 'https://firebasestorage.googleapis.com/v0/b/kcyc-site.firebasestorage.app/o/LOGO%20KCYC%2FGroup%206680.png?alt=media&token=2da4c663-70f3-4415-90bf-458feae55e30';
     }
   };
 
